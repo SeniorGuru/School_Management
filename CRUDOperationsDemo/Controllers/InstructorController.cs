9fa4bb4a0ad69eaa481c7058f09126cfd45d8856
SeniorNet <109102047+SeniorNet@users.noreply.github.com>
2022-08-09 19:28:06 -0400

    Add Instructor Controller

--- CRUDOperationsDemo/Controllers/InstructorController.cs
+++ CRUDOperationsDemo/Controllers/InstructorController.cs
@@ -2,6 +2,7 @@
 using CRUDOperationsDemo.Models;
 using Microsoft.AspNetCore.Mvc;
 using School.Models;
+using System.Linq;
 
 namespace School.Controllers
 {
@@ -16,20 +17,43 @@ namespace School.Controllers
             this.env = env;
         }
 
-        public IActionResult Index()
+        public SchoolDbContext Get_context()
         {
+            return _context;
+        }
+
+        public IActionResult Index(SchoolDbContext _context)
+        {
+            string teacherName = TempData["teacherName"] as string;
+
             InstructorViewModel instructor = new InstructorViewModel();
             instructor.subjects = new List<Subject>();
             instructor.tsubject = new List<Subject>();
             instructor.users = new List<User>();
             instructor.teacher = new User();
 
-            instructor.teacher = _context.users.FirstOrDefault(a => a.Title == "teacher");
+            List<User> users = new List<User>();
+            List<Subject> tsubjects = new List<Subject>();
+
             instructor.subjects = _context.subjects;
             instructor.users = _context.users;
 
-            string teacherName = instructor.teacher.FirstName + " " + instructor.teacher.LastName;
-            instructor.tsubject = _context.enrolls.SelectMany(a => a.Teacher == teacherName);
+            if(teacherName == null)
+            {
+                instructor.teacher = _context.users.FirstOrDefault(a => a.Title == "teacher");
+                teacherName = instructor.teacher.FirstName + " " + instructor.teacher.LastName;
+            }
+            foreach(var item in _context.enrolls)
+            {
+                if(item.Teacher == teacherName)
+                {
+                    Subject temp = new Subject();
+                    temp.Name = item.Subject;
+                    tsubjects.Add(temp);
+                }
+            }
+
+            instructor.tsubject = (IEnumerable<Subject>)tsubjects;
             return View(instructor);
         }
 
